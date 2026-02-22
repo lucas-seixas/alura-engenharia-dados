fc8753eeb8f53a692500e4f1610733bfd344f023
lucas-seixas <lseixasd@gmail.com>
2026-02-21 21:15:28 -0300

    Refatora encapsulamento e métodos privados da classe Dados
    Melhora a estrutura de encapsulamento, tornando métodos e atributos internos privados. Aumenta a robustez, previne acessos indevidos aos dados internos e facilita manutenção futura. Centraliza lógica de transformação de dados, reduz redundância e potencializa reutilização. Adiciona clareza na interface pública e separa responsabilidades.

--- src/ed_alura/modulo_01_pipeline_dados/processamento_dados.py
+++ src/ed_alura/modulo_01_pipeline_dados/processamento_dados.py
@@ -4,79 +4,78 @@ import json
 
 class Dados:
     def __init__(self, path, tipo_dados):
-        self.path = path
-        self.tipo_dados = tipo_dados
-        self.dados = self.leitura_dados()
-        self.nome_colunas = self.get_columns()
-        self.qtd_linhas = self.size_data()
+        self.__path = path
+        self.__tipo_dados = tipo_dados
+        self.dados = self.__leitura_dados()
+        self.nome_colunas = self.__get_columns()
+        self.qtd_linhas = self.__size_data()
 
-    def leitura_json(self):
+    def __leitura_json(self):
         dados_json = []
-        with open(self.path) as file:
+        with open(self.__path) as file:
             dados_json = json.load(file)
         return dados_json
 
-    def leitura_csv(self):
+    def __leitura_csv(self):
         dados_csv = []
-        with open(self.path) as file:
+        with open(self.__path) as file:
             spamreader = csv.DictReader(file, delimiter=",")
             [dados_csv.append(row) for row in spamreader]
         return dados_csv
 
-    def leitura_dados(self):
+    def __leitura_dados(self):
         dados = []
 
-        if self.tipo_dados == "json":
-            dados = self.leitura_json()
+        if self.__tipo_dados == "json":
+            dados = self.__leitura_json()
 
-        elif self.tipo_dados == "csv":
-            dados = self.leitura_csv()
+        elif self.__tipo_dados == "csv":
+            dados = self.__leitura_csv()
 
-        elif self.tipo_dados == "list":
-            dados = self.path
-            self.path = "lista em memoria"
+        elif self.__tipo_dados == "list":
+            dados = self.__path
+            self.__path = "lista em memoria"
 
         else:
             raise ValueError("Tipo de arquivo não suportado")
 
         return dados
 
-    def get_columns(self):
+    def __get_columns(self):
         return list(self.dados[-1].keys())
 
+    def __size_data(self):
+        return len(self.dados)
+
+    def __transformando_dados_tabela(self):
+        dados_combinados_tabela = [self.nome_colunas]
+
+        for row in self.dados:
+            linha = []
+            for coluna in self.nome_colunas:
+                linha.append(row.get(coluna, "Indisponível"))
+            dados_combinados_tabela.append(linha)
+
+        return dados_combinados_tabela
+
+    # Métodos públicos
     def rename_columns(self, key_mapping):
-        new_dados = []
         new_dados = [
             {key_mapping[old_key]: value for old_key, value in old_dict.items()}
             for old_dict in self.dados
         ]
         self.dados = new_dados
-        self.nome_colunas = self.get_columns()
+        self.nome_colunas = self.__get_columns()
 
-    def size_data(self):
-        return len(self.dados)
-
+    @staticmethod
     def join(dadosA, dadosB):
         combined_list = []
         combined_list.extend(dadosA.dados)
         combined_list.extend(dadosB.dados)
         return Dados(combined_list, "list")
 
-    def transformando_dados_tabela(self):
-        dados_combinados_tabela = [self.nome_colunas]
-
-        for row in self.dados:
-            linha = []
-            for coluna in self.nome_colunas:
-                # Retornar indisponível caso a chave não exista, evitando erros
-                linha.append(row.get(coluna, "Indisponível"))
-            dados_combinados_tabela.append(linha)
-
-        return dados_combinados_tabela
-
     def salvando_dados(self, path):
-
-        dados_combinados_tabela = self.transformando_dados_tabela()
+        dados_combinados_tabela = self.__transformando_dados_tabela()
 
         with open(path, "w") as file:
             writer = csv.writer(file)
